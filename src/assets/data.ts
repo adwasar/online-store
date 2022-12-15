f2c66a3fb37b9fefe1d1da1fde278d10b50ff7c6
pnmrvvtl <pnmrv.vtl@gmail.com>
2022-12-15 18:45:39 +0200

    feat: create data object for online shop

---
+++ src/assets/data.ts
@@ -0,0 +1,246 @@
+const detailsData = [
+    [
+        {
+            id: 1,
+            name: 'Бендикс 2101, 2102, 2103, 2104, 2105, 2106, 2107 Авто-Электрика (привод стартера)_',
+            quantity: 10,
+            price: 139.73,
+            description: 'Число зубцов 11',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6812/0010.jpg', 'https://detali.zp.ua/uploads/photo/3d/6812/0001.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_06/2018_06_30_I6G_142754.jpg']
+        },
+        {
+            id: 2,
+            name: 'Бендикс 2101, 2102, 2103, 2104, 2105, 2106, 2107 Авто-Электрика для БАТЭ 425.3708',
+            quantity: 5,
+            price: 186.30,
+            description: 'Число зубцов 11',
+            images: ['https://detali.zp.ua/uploads/photo/3d/18345/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/18345/0010.jpg', 'https://detali.zp.ua/uploads/photo/big/2020_06/2020_06_27_I6G_121153.jpg']
+        },
+        {
+            id: 3,
+            name: 'Бендикс 2108, 2109, 21099, 2113, 2114, 2115 Авто-Электрика (привод стартера)_',
+            quantity: 12,
+            price: 128.08,
+            description: 'Число зубцов 11',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6813/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6813/0010.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_07/2018_07_08_I7G_105704.jpg']
+        },
+        {
+            id: 4,
+            name: 'Бендикс 2108, 2109, 21099, 2113, 2114, 2115 Авто-Электрика для БАТЭ',
+            quantity: 10,
+            price: 186.30,
+            description: 'Число зубцов 11',
+            images: ['https://detali.zp.ua/uploads/photo/3d/18346/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/18346/0010.jpg', 'https://detali.zp.ua/uploads/photo/big/2020_06/2020_06_27_I6G_121232.jpg']
+        },
+        {
+            id: 5,
+            name: 'Бендикс 402 двигатель малый Авто-Электрика',
+            quantity: 7,
+            price: 209.59,
+            description: 'Число зубцов 9',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6815/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6815/0010.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_07/2018_07_08_I7G_160055.jpg']
+        },
+        {
+            id: 6,
+            name: 'Бендикс 406 двигатель 3302, 2217, 2705, 3110 Авто-Электрика',
+            quantity: 10,
+            price: 186.30,
+            description: 'Число зубцов 9',
+            images: ['https://detali.zp.ua/uploads/photo/3d/18341/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/18341/0010.jpg', 'https://detali.zp.ua/uploads/photo/big/2020_06/2020_06_27_I6G_121019.jpg']
+        },
+        {
+            id: 7,
+            name: 'Бендикс Таврия, 1102, 1103, 1105 Авто-Электрика',
+            quantity: 8,
+            price: 139.73,
+            description: 'Число зубцов 9',
+            images: ['https://detali.zp.ua/uploads/photo/3d/18339/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/18339/0010.jpg', 'https://detali.zp.ua/uploads/photo/big/2020_06/2020_06_27_I6G_121000.jpg']
+        },
+        {
+            id: 8,
+            name: 'Бендикс большой ГАЗ-53 Авто-Электрика (привод стартера)',
+            quantity: 10,
+            price: 186.30,
+            description: 'Число зубцов 9',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6814/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6814/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_03/2018_03_31_I3G_162300.jpg']
+        },
+    ],
+
+    [
+        {
+            id: 1,
+            name: 'Щетка генератора 2 контактная в сборе Волга, ГАЗ Авто-Электрика',
+            quantity: 16,
+            price: 48.90,
+            description: 'Рабочее напряжение 12-14,4В',
+            images: ['https://detali.zp.ua/uploads/photo/3d/15998/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/15998/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2020_10/2020_10_29_I10G_140509.jpg']
+        },
+        {
+            id: 2,
+            name: 'Щетка генератора 2101, 2102, 2103, 2104, 2106, 2107 Авто-Электрика',
+            quantity: 8,
+            price: 34.93,
+            description: 'Рабочее напряжение 12-14,4В',
+            images: ['https://detali.zp.ua/uploads/photo/3d/7001/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/7001/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_07/2018_07_08_I7G_141308.jpg']
+        },
+        {
+            id: 3,
+            name: 'Щетка генератора 1 контактная в сборе Москвич, РАФ, ПАЗ, УАЗ, ЛАЗ Авто-Электрика',
+            quantity: 10,
+            price: 48.90,
+            description: 'Рабочее напряжение 12-14,4В',
+            images: ['https://detali.zp.ua/uploads/photo/3d/15997/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/15997/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2020_09/2020_09_26_I9G_085910.jpg']
+        },
+        {
+            id: 4,
+            name: 'Щетка генератора 2104, 2105, 2107 Авто-Электрика',
+            quantity: 17,
+            price: 30.27,
+            description: 'Рабочее напряжение 12-14,4В',
+            images: ['https://detali.zp.ua/uploads/photo/3d/15995/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/15995/0010.jpg', 'https://detali.zp.ua/uploads/photo/big/2020_05/2020_05_10_I5G_111437.jpg']
+        },
+        {
+            id: 5,
+            name: 'Щетка генератора 2108, 2109, 21099 Авто-Электрика',
+            quantity: 10,
+            price: 30.27,
+            description: 'Рабочее напряжение 12-14,4В',
+            images: ['https://detali.zp.ua/uploads/photo/3d/15996/0019.jpg', 'https://detali.zp.ua/uploads/photo/3d/15996/0001.jpg', 'https://detali.zp.ua/uploads/photo/big/2020_05/2020_05_10_I5G_111413.jpg']
+        },
+        {
+            id: 6,
+            name: 'Регулятор напряжения 2108 с щетками Авто-Электрика',
+            quantity: 3,
+            price: 97.03,
+            description: 'Рабочее напряжение 12-14,4В',
+            images: ['https://detali.zp.ua/uploads/photo/3d/15926/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/15926/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2020_04/2020_04_21_I4G_160146.jpg']
+        },
+        {
+            id: 7,
+            name: 'Регулятор напряжения 2110, 2111, 2112 с щетками (интегралка, реле зарядки, шоколадка) Авто-Электрика',
+            quantity: 10,
+            price: 76.33,
+            description: 'Рабочее напряжение 12-14,4В',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6825/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6825/0010.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_03/2018_03_31_I3G_140816.jpg']
+        },
+    ],
+
+    [
+        {
+            id: 1,
+            name: 'Стартер Ланос, Авео, Лачетти, Нексия, Эсперо, Такума, Нубира 1,5 - 1,6 двигатели Авто-Электрика редукторный 10 зубьев',
+            quantity: 3,
+            price: 2067.41,
+            description: 'Напряжение, В: 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6811/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6811/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_03/2018_03_31_I3G_154918.jpg']
+        },
+        {
+            id: 2,
+            name: 'Стартер 2108, 2109, 2113, 2114, 2115 Авто-Электрика (на постоянных магнитах с редуктором)',
+            quantity: 10,
+            price: 2067.41,
+            description: 'Напряжение, В: 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/7693/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/7693/0010.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_07/2018_07_05_I7G_133134.jpg']
+        },
+        {
+            id: 3,
+            name: 'Стартер 2110, 2111, 2112, 1117, 1118, 1119 Авто-Электрика (на постоянных магнитах с редуктором)',
+            quantity: 2,
+            price: 2067.41,
+            description: 'Напряжение, В: 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6807/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6807/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_03/2018_03_31_I3G_101809.jpg']
+        },
+        {
+            id: 4,
+            name: 'Стартер 402 двигатель малый Авто-Электрика (на постоянный магнитах с редукторм)',
+            quantity: 7,
+            price: 2112.69,
+            description: 'Напряжение, В: 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6809/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6809/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_03/2018_03_31_I3G_141620.jpg']
+        },
+        {
+            id: 5,
+            name: 'Стартер 405, 406 двигатель Авто-Электрика (на постоянный магнитах с редуктором)',
+            quantity: 4,
+            price: 2112.69,
+            description: 'Напряжение, В: 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6808/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6808/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_03/2018_03_29_I3G_121519.jpg']
+        },
+        {
+            id: 6,
+            name: 'Стартер Таврия, 1102, 1103, 1105, Сенс Авто-Электрика (на постоянных магнитах)',
+            quantity: 10,
+            price: 2067.41,
+            description: 'Напряжение, В: 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6810/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6810/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_07/2018_07_28_I7G_155121.jpg']
+        },
+        {
+            id: 7,
+            name: 'Стартер 2101, 2102, 2103, 2104, 2105, 2106, 2107, 2121, 21213, 21214, 2123 AT (на постоянных магнитах с редуктором)',
+            quantity: 4,
+            price: 2067.41,
+            description: 'Напряжение, В: 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6804/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6804/0010.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_04/2018_04_12_I4G_162837.jpg']
+        },
+    ],
+    [
+        {
+            id: 1,
+            name: 'Генератор 21214 100А Авто-Электрика',
+            quantity: 3,
+            price: 3156.75,
+            description: 'Сила тока, А - 100, Напряжение, В - 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/28393/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/28393/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2022_11/2022_11_04_I11G_084317.jpg']
+        },
+        {
+            id: 2,
+            name: 'Генератор 2108, 2109, 21099, 2110, 2111, 2112 100А инжекторный двигатель Авто-Электрика',
+            quantity: 2,
+            price: 3155.46,
+            description: 'Сила тока, А - 100, Напряжение, В - 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6179/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6179/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_07/2018_07_14_I7G_134846.jpg']
+        },
+        {
+            id: 3,
+            name: 'Генератор 2123 100А Авто-Электрика',
+            quantity: 3,
+            price: 3117.94,
+            description: 'Сила тока, А - 100, Напряжение, В - 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/28383/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/28383/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2022_11/2022_11_04_I11G_084401.jpg']
+        },
+        {
+            id: 4,
+            name: 'Генератор 1102, 1103, 1105, Сенс 65А Авто-Электрика',
+            quantity: 3,
+            price: 2643.13,
+            description: 'Сила тока, А - 65, Напряжение, В - 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6243/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6243/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_03/2018_03_31_I3G_123340.jpg']
+        },
+        {
+            id: 5,
+            name: 'Генератор 2108, 2109, 21099, 2110, 2111 73А Авто-Электрика',
+            quantity: 3,
+            price: 2432.25,
+            description: 'Сила тока, А - 73, Напряжение, В - 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6178/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6178/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_07/2018_07_11_I7G_085404.jpg']
+        },
+        {
+            id: 6,
+            name: 'Генератор 2104, 2105, 2107 73А Авто-Электрика',
+            quantity: 3,
+            price: 2419.31,
+            description: 'Сила тока, А - 73, Напряжение, В - 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6177/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6177/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_07/2018_07_08_I7G_133458.jpg']
+        },
+        {
+            id: 7,
+            name: 'Генератор 2101, 2102, 2103, 2104,2106, 2107 55А Авто-Электрика',
+            quantity: 3,
+            price: 2251.13,
+            description: 'Сила тока, А - 55, Напряжение, В - 12',
+            images: ['https://detali.zp.ua/uploads/photo/3d/6176/0001.jpg', 'https://detali.zp.ua/uploads/photo/3d/6176/0019.jpg', 'https://detali.zp.ua/uploads/photo/big/2018_07/2018_07_03_I7G_122144.jpg']
+        },
+    ]
+];
+
+export default detailsData;
