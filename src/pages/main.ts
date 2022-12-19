fe7f4a3509f1b2f51db45145f7f2b66bec8e843b
Vladislav <104913853+Adwasar@users.noreply.github.com>
2022-12-19 18:59:31 +0200

    feat: create cards generation(#6)
    * feat: rendoring leyout items with script
    
    * feat: render product cards with corret info
    
    * style: change product catds cize
    
    * docs: remove unnesessory commits
    
    Co-authored-by: Adwasar <user@Users-MacBook-Pro.local>

--- src/pages/main.ts
+++ src/pages/main.ts
@@ -1,14 +1,28 @@
 import {Product} from "../types/product";
 
-export function renderMainPage(products: Product[]): HTMLElement {
-    const mainPage = document.createElement('div');
+const productsAria = document.querySelector('.products__items') as HTMLElement;
+
+export function renderMainPage(products: Product[]): void {
     for (let i = 0; i < products.length; i++) {
-        //create product card and add to mainPageDiv
-        //use this code
-        // const productCard = document.createElement('div');
-        // productCard.dataset.index = String(products[i].id);
-        // /////work with dom and css classes of product card
-        // mainPage.appendChild(productCard);
+        productsAria.innerHTML += `
+            <div class="products__item">
+              <div class="products__item-wrapper">
+                <div class="products__item-text">
+                  <div class="products__item-title">${products[i].name}</div>
+                  <img src=${products[i].images[0]} class="products__item-img"></img>
+                  <div class="products__item-info">
+                    <p><b>Категория:</b> ${products[i].category}</p>
+                    <p><b>Производитель:</b> ${products[i].brand}</p>
+                    <p><b>Цена:</b> ${products[i].price} грн</p>
+                    <p><b>В наличии:</b> 8шт</p>
+                  </div>
+                </div>
+                <div class="products__item-buttons">
+                  <div class="products__buttons-in-cart">В корзину</div>
+                  <div class="products__buttons-details">Подробнее</div>
+                </div>
+              </div>
+            </div>
+        `
     }
-    return mainPage;
 }
