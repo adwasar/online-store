66e9518e0f0e49aa673d07d6e146cb4593821526
pnmrvvtl <pnmrv.vtl@gmail.com>
2022-12-19 15:53:08 +0200

    style: add styling to product page

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import './index.html';
 import './scss/main.scss';
+import {renderProductPage} from "./pages/product";
+import detailsData from "./assets/data";
 
+Array.from(document.getElementsByClassName('app-store-page') as HTMLCollectionOf<HTMLElement>)[0].style.display = 'none';
+document.querySelector('main')?.querySelector('.wrapper')?.appendChild(renderProductPage(3, detailsData));
