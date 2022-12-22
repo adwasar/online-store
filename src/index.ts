f0b63de882c14e1a62f1315a4667dea6201ad56a
pnmrvvtl <pnmrv.vtl@gmail.com>
2022-12-22 21:15:50 +0200

    feat: create initial values for filters elements

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,81 @@ import logo from './assets/img/logo.png';
 import cart from './assets/img/cart.png';
 import {renderCartPage} from "./pages/cart";
 
+const minPriceInput = document.querySelector('#from-slider-price') as HTMLInputElement;
+const maxPriceInput = document.querySelector('#to-slider-price') as HTMLInputElement;
+const minPriceDiv = document.querySelector('.filters__price-min-value') as HTMLElement;
+const maxPriceDiv = document.querySelector('.filters__price-max-value') as HTMLElement;
+const minStockInput = document.querySelector('#from-slider-stock') as HTMLInputElement;
+const maxStockInput = document.querySelector('#to-slider-stock') as HTMLInputElement;
+const minStockDiv = document.querySelector('.filters__stock-min-value') as HTMLElement;
+const maxStockDiv = document.querySelector('.filters__stock-max-value') as HTMLElement;
+
+minPriceInput.oninput = onFiltersValueChanged;
+maxPriceInput.oninput = onFiltersValueChanged;
+minStockInput.oninput = onFiltersValueChanged;
+maxStockInput.oninput = onFiltersValueChanged;
+
+function onFiltersValueChanged(event: Event) {
+    const eventTarget = (event.target as HTMLInputElement);
+    switch (eventTarget.id) {
+        case 'from-slider-price':
+            if (Number(eventTarget.value) >= Number(maxPriceInput.value)) {
+                eventTarget.value = String(Number(maxPriceInput.value) - 1);
+                break;
+            }
+            minPriceDiv.innerHTML = eventTarget.value;
+            break;
+        case 'to-slider-price':
+            if (Number(eventTarget.value) <= Number(minPriceInput.value)) {
+                eventTarget.value = String(Number(minPriceInput.value) + 1);
+                break;
+            }
+            maxPriceDiv.innerHTML = eventTarget.value;
+            break;
+        case 'from-slider-stock':
+            if (Number(eventTarget.value) >= Number(maxStockInput.value)) {
+                eventTarget.value = String(Number(maxStockInput.value) - 1);
+                break;
+            }
+            minStockDiv.innerHTML = eventTarget.value;
+            break;
+        case 'to-slider-stock':
+            if (Number(eventTarget.value) <= Number( minStockInput.value)) {
+                eventTarget.value = String(Number(minStockInput.value) + 1);
+                break;
+            }
+            maxStockDiv.innerHTML = eventTarget.value;
+            break;
+    }
+}
+
+function initializeAllInputs() {
+    const minPrice = detailsData.reduce((acc, el) => el.price < acc ? el.price : acc, Infinity);
+    const maxPrice = detailsData.reduce((acc, el) => el.price > acc ? el.price : acc, -Infinity);
+    const minStock = detailsData.reduce((acc, el) => el.quantity < acc ? el.quantity : acc, Infinity);
+    const maxStock = detailsData.reduce((acc, el) => el.quantity > acc ? el.quantity : acc, -Infinity);
+    minPriceDiv.innerHTML = String(minPrice);
+    maxPriceDiv.innerHTML = String(maxPrice);
+    minStockDiv.innerHTML = String(minStock);
+    maxStockDiv.innerHTML = String(maxStock);
+    minPriceInput.min = String(minPrice);
+    maxPriceInput.min = String(minPrice);
+    minStockInput.min = String(minStock);
+    maxStockInput.min = String(minStock);
+
+    minPriceInput.max = String(maxPrice);
+    maxPriceInput.max = String(maxPrice);
+    minStockInput.max = String(maxStock);
+    maxStockInput.max = String(maxStock);
+
+    minPriceInput.value = String(minPrice);
+    maxPriceInput.value = String(maxPrice);
+    minStockInput.value = String(minStock);
+    maxStockInput.value = String(maxStock);
+}
+
+initializeAllInputs();
+
 function hideAllElements() {
     (document.querySelector('.app-store-page') as HTMLElement).style.display = 'none';
     document.querySelector('.products__items')?.remove();
@@ -22,14 +97,14 @@ logoElement.onclick = () => {
     const productsPage = renderMainPage(detailsData);
     productsPage.classList.add('products__items');
     document.querySelector('.products')?.appendChild(productsPage);
-    window.history.pushState({},'',`/`);
+    window.history.pushState({}, '', `/`);
 }
 const cartElement = document.querySelector('#cart') as HTMLImageElement;
 cartElement.src = cart;
 cartElement.onclick = () => {
     hideAllElements();
     document.querySelector('main')?.querySelector('.wrapper')?.appendChild(renderCartPage());
-    window.history.pushState({},'',`/cart/`);
+    window.history.pushState({}, '', `/cart/`);
 }
 
 const searchString = window.location.search;
@@ -48,7 +123,7 @@ if (!searchString && pathString === '/') {
             document.querySelector('main')?.querySelector('.wrapper')?.appendChild(renderCartPage());
         } else {
             if (RegExp(/\/product\/\d*/i).test(pathString)) {
-                const productMatch = pathString.match(/\/product\/(\d*)/i) || [0,1];
+                const productMatch = pathString.match(/\/product\/(\d*)/i) || [0, 1];
                 const productId = Number(productMatch[1]);
                 console.log(`productId = ${productId}`);
                 if (productId >= 1 && productId < detailsData.length + 1) {
